49b6f199fef0a256629a30ded732fd6b45682961
BakiMK <AobakweMakhoba@hotmail.com>
2023-07-10 09:44:20 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -4,7 +4,7 @@
  * str_concat - get ends of input and add together for size
  * @s1: input one to concat
  * @s2: input two to concat
- * 
+ *
  * Return: conct
  */
 char *str_concat(char *s1, char *s2)
@@ -39,6 +39,6 @@ char *str_concat(char *s1, char *s2)
 		b++, ci++;
 	}
 	conct[b] = '\0';
-	
+
 	return (conct);
 }
